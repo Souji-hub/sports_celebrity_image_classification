1145a9c1cfc8382fd74778259d8dc1ec47760b34
Souji-hub <idebussoujan@gmail.com>
2024-01-05 16:43:12 +0545

    Server_creation

---
+++ server/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/classify_image', methods = ['GET','POST'])
+def classify_image():
+    return "Hello_World"
+
+if __name__=="__main__":
+    app.run(port=5000, debug=True)
+
